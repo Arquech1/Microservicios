da5700a539315c7c75974e1c0c746958ada10202
Arquech1 <arquecho@gmail.com>
2024-11-18 20:25:16 -0400

    Microservicio de booking

---
+++ booking/docker/data.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS booking;
+
+USE booking;
+
+CREATE TABLE booking(
+
+
+
+)
---
+++ booking/src/test/java/com/productos/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productos.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
